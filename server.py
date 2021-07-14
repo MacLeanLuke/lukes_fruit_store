39af0b1419b2f550feb28e168db0a0eac8e1ffaf
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-07-14 07:37:22 -0500

    deleting print statement

--- server.py
+++ server.py
@@ -12,8 +12,6 @@ def checkout():
 
     amount = int(request.form["strawberry"]) + int(request.form["raspberry"]) + int(request.form["apple"])
 
-    print(f'Charging {request.form["first_name"]} {request.form["last_name"]} for {amount} fruits')
-
     return render_template("checkout.html", strawberry=request.form["strawberry"], raspberry=request.form["raspberry"], apple=request.form["apple"], firstName=request.form["first_name"], lastName=request.form["last_name"], message=request.form["message"])
 
 @app.route('/fruits')         
